53837224fbcd94be767f6592cb1983a2a0f79ae2
drexocoder-source <drexocoder@gmail.com>
2026-03-05 22:37:22 +0530

    Update maintenance.py

--- ShrutiMusic/plugins/sudo/maintenance.py
+++ ShrutiMusic/plugins/sudo/maintenance.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
 
 
 from pyrogram import filters
@@ -61,13 +41,4 @@ async def maintenance(client, message: Message):
         await message.reply_text(usage)
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
 
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
